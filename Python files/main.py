1b82cdef4f4eaa666d9416f0c0a318f198a09581
tennika20 <90783474+tennika20@users.noreply.github.com>
2022-05-07 04:17:22 -0500

    updated main

--- Python files/main.py
+++ Python files/main.py
@@ -1,19 +1,26 @@
 import os
 import re
+import time 
 from partthree import *
 from assert_agg import *
 from gen_data_pre import *
+from generate_plot import *
+import subprocess 
 
 
 
 if __name__=="__main__":
-    os.system('rm -rf csv_data')
-    os.system('mkdir csv_data')
+    csv_files_rm=os.system('rm -rf csv_data')
+    csv_files_dir=os.system('mkdir csv_data')
+    figures_rm=os.system('rm -rf Figures')
+    figures_dir=os.system('mkdir Figures')
     GEN_CSV_Assert()
     ASSERTNDEBUG()
     assertdebugCSV()
     assert_aggCSV()
-
-    
-
-
+    subprocess.run(['python','./PythonFiles/PyDrillerScript.py'])
+    outDrillerCSV()
+    os.system(figures_rm)
+    os.system(figures_dir)
+    subprocess.run(['python','./PythonFiles/pydriller.py'])
+   
